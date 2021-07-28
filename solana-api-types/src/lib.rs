dd9d420f4c66a423c8881f72e019ce01260eeef0
Dmitry Shlagoff <richyannet@yandex.ru>
2021-07-28 15:10:20 +0700

    Add all the required trait methods

--- solana-api-types/src/lib.rs
+++ solana-api-types/src/lib.rs
@@ -1,7 +1,14 @@
 use async_trait::async_trait;
 use serde_json::Value;
 use solana_sdk::{
-    account::Account, pubkey::Pubkey, signature::Signature, transaction::Transaction,
+    account::Account,
+    clock::{Slot, UnixTimestamp},
+    commitment_config::{CommitmentConfig, CommitmentLevel},
+    instruction::CompiledInstruction,
+    message::MessageHeader,
+    pubkey::Pubkey,
+    signature::Signature,
+    transaction::{self, Transaction, TransactionError},
 };
 
 use serde::{Deserialize, Serialize};
@@ -29,8 +36,6 @@ pub struct UiAccount {
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase", untagged)]
 pub enum UiAccountData {
-    /// Legacy. Retained for RPC backwards compatibility
-    LegacyBinary(String),
     Json(ParsedAccount),
     Binary(String, UiAccountEncoding),
 }
@@ -38,13 +43,22 @@ pub enum UiAccountData {
 #[derive(Serialize, Deserialize, Clone, Copy, Debug, PartialEq)]
 #[serde(rename_all = "camelCase")]
 pub enum UiAccountEncoding {
-    /// Legacy. Retained for RPC backwards compatibility
-    Binary,
+    Binary, // Legacy. Retained for RPC backwards compatibility
     Base58,
     Base64,
     JsonParsed,
     #[serde(rename = "base64+zstd")]
     Base64Zstd,
+}
+
+#[derive(Serialize, Deserialize, Clone, Copy, Debug, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub enum UiTransactionEncoding {
+    Binary, // Legacy. Retained for RPC backwards compatibility
+    Base64,
+    Base58,
+    Json,
+    JsonParsed,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -58,33 +72,322 @@ pub enum ErrorCode {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub enum CommitmentConfig {
-    Finalized,
-    Confirmed,
-    Processed,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
 pub struct Error {
     pub code: ErrorCode,
     pub message: String,
     pub data: Option<Value>,
 }
 
-struct AccountSliceConfig {
-    offset: u64,
-    length: u64,
+#[derive(Clone, Copy, Debug, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct UiDataSliceConfig {
+    pub offset: usize,
+    pub length: usize,
 }
 
-/// https://docs.solana.com/developing/clients/jsonrpc-api#filters
-struct Memcmp {
-    offset: u64,
-    bytes: String,
+/// Configuration object for
+/// [getAccountInfo](https://docs.solana.com/developing/clients/jsonrpc-api#getaccountinfo) request.
+#[derive(Debug, Clone, Default, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RpcAccountInfoConfig {
+    pub encoding: Option<UiAccountEncoding>,
+    pub data_slice: Option<UiDataSliceConfig>,
+    #[serde(flatten)]
+    pub commitment: Option<CommitmentConfig>,
 }
 
-struct AccountFilter {
-    data_size: u64,
-    memcmp: Memcmp,
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub enum MemcmpEncoding {
+    Binary,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase", untagged)]
+pub enum MemcmpEncodedBytes {
+    Binary(String),
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Memcmp {
+    /// Data offset to begin match
+    pub offset: usize,
+    /// Bytes, encoded with specified encoding, or default Binary
+    pub bytes: MemcmpEncodedBytes,
+    /// Optional encoding specification
+    pub encoding: Option<MemcmpEncoding>,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub enum RpcFilterType {
+    DataSize(u64),
+    Memcmp(Memcmp),
+}
+
+#[derive(Debug, Clone, Default, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RpcProgramAccountsConfig {
+    pub filters: Option<Vec<RpcFilterType>>,
+    #[serde(flatten)]
+    pub account_config: RpcAccountInfoConfig,
+    pub with_context: Option<bool>,
+}
+
+pub struct RpcKeyedAccount {
+    pub pubkey: String,
+    pub account: Account,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RpcSignatureStatusConfig {
+    pub search_transaction_history: bool,
+}
+
+#[derive(Debug, Clone, Default, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RpcSignaturesForAddressConfig {
+    pub before: Option<String>, // Signature as base-58 string
+    pub until: Option<String>,  // Signature as base-58 string
+    pub limit: Option<usize>,
+    #[serde(flatten)]
+    pub commitment: Option<CommitmentConfig>,
+}
+
+#[derive(Debug, Clone, Default, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SignatureInfo {
+    pub signature: String,
+    pub slog: u64,
+    pub err: Option<TransactionError>,
+    pub memo: Option<String>,
+    pub block_time: Option<i64>,
+}
+
+#[derive(Debug, Clone, Default, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RpcSlotConfig {
+    #[serde(flatten)]
+    pub commitment: Option<CommitmentConfig>,
+}
+
+#[derive(Debug, Clone, Copy, Default, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RpcTransactionConfig {
+    pub encoding: Option<UiTransactionEncoding>,
+    #[serde(flatten)]
+    pub commitment: Option<CommitmentConfig>,
+}
+
+/// A duplicate representation of an Instruction for pretty JSON serialization
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase", untagged)]
+pub enum UiInstruction {
+    Compiled(UiCompiledInstruction),
+    Parsed(UiParsedInstruction),
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct ParsedInstruction {
+    pub program: String,
+    pub program_id: String,
+    pub parsed: Value,
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase", untagged)]
+pub enum UiParsedInstruction {
+    Parsed(ParsedInstruction),
+    PartiallyDecoded(UiPartiallyDecodedInstruction),
+}
+
+/// A partially decoded CompiledInstruction that includes explicit account addresses
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct UiPartiallyDecodedInstruction {
+    pub program_id: String,
+    pub accounts: Vec<String>,
+    pub data: String,
+}
+
+/// A duplicate representation of a CompiledInstruction for pretty JSON serialization
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct UiCompiledInstruction {
+    pub program_id_index: u8,
+    pub accounts: Vec<u8>,
+    pub data: String,
+}
+
+impl From<&CompiledInstruction> for UiCompiledInstruction {
+    fn from(instruction: &CompiledInstruction) -> Self {
+        Self {
+            program_id_index: instruction.program_id_index,
+            accounts: instruction.accounts.clone(),
+            data: bs58::encode(instruction.data.clone()).into_string(),
+        }
+    }
+}
+
+/// A duplicate representation of a Message, in raw format, for pretty JSON serialization
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct UiRawMessage {
+    pub header: MessageHeader,
+    pub account_keys: Vec<String>,
+    pub recent_blockhash: String,
+    pub instructions: Vec<UiCompiledInstruction>,
+}
+
+/// A duplicate representation of a Message, in parsed format, for pretty JSON serialization
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct UiParsedMessage {
+    pub account_keys: Vec<ParsedAccount>,
+    pub recent_blockhash: String,
+    pub instructions: Vec<UiInstruction>,
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase", untagged)]
+pub enum UiMessage {
+    Parsed(UiParsedMessage),
+    Raw(UiRawMessage),
+}
+
+/// A duplicate representation of a Transaction for pretty JSON serialization
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct UiTransaction {
+    pub signatures: Vec<String>,
+    pub message: UiMessage,
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase", untagged)]
+pub enum EncodedTransaction {
+    LegacyBinary(String), // Old way of expressing base-58, retained for RPC backwards compatibility
+    Binary(String, UiTransactionEncoding),
+    Json(UiTransaction),
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct EncodedTransactionWithStatusMeta {
+    pub transaction: EncodedTransaction,
+    pub meta: Option<UiTransactionStatusMeta>,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct EncodedConfirmedTransaction {
+    pub slot: Slot,
+    #[serde(flatten)]
+    pub transaction: EncodedTransactionWithStatusMeta,
+    pub block_time: Option<UnixTimestamp>,
+}
+
+/// A duplicate representation of TransactionStatusMeta with `err` field
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct UiTransactionStatusMeta {
+    pub err: Option<TransactionError>,
+    pub status: transaction::Result<()>, // This field is deprecated.  See https://github.com/solana-labs/solana/issues/9302
+    pub fee: u64,
+    pub pre_balances: Vec<u64>,
+    pub post_balances: Vec<u64>,
+    pub inner_instructions: Option<Vec<UiInnerInstructions>>,
+    pub log_messages: Option<Vec<String>>,
+    pub pre_token_balances: Option<Vec<UiTransactionTokenBalance>>,
+    pub post_token_balances: Option<Vec<UiTransactionTokenBalance>>,
+    pub rewards: Option<Rewards>,
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct UiInnerInstructions {
+    /// Transaction instruction index
+    pub index: u8,
+    /// List of inner instructions
+    pub instructions: Vec<UiInstruction>,
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct UiTransactionTokenBalance {
+    pub account_index: u8,
+    pub mint: String,
+    pub ui_token_amount: UiTokenAmount,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize, Clone, Copy)]
+pub enum RewardType {
+    Fee,
+    Rent,
+    Staking,
+    Voting,
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Reward {
+    pub pubkey: String,
+    pub lamports: i64,
+    pub post_balance: u64, // Account balance in lamports after `lamports` was applied
+    pub reward_type: Option<RewardType>,
+    pub commission: Option<u8>, // Vote account commission when the reward was credited, only present for voting and staking rewards
+}
+
+pub type StringAmount = String;
+pub type StringDecimals = String;
+pub type Rewards = Vec<Reward>;
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct UiTokenAmount {
+    pub ui_amount: Option<f64>,
+    pub decimals: u8,
+    pub amount: StringAmount,
+    pub ui_amount_string: StringDecimals,
+}
+
+#[derive(Debug, Default, Clone, Copy, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RpcSendTransactionConfig {
+    #[serde(default)]
+    pub skip_preflight: bool,
+    pub preflight_commitment: Option<CommitmentLevel>,
+    pub encoding: Option<UiTransactionEncoding>,
+}
+
+#[derive(Debug, Clone, Default, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RpcSimulateTransactionAccountsConfig {
+    pub encoding: Option<UiAccountEncoding>,
+    pub addresses: Vec<String>,
+}
+
+#[derive(Debug, Default, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RpcSimulateTransactionConfig {
+    #[serde(default)]
+    pub sig_verify: bool,
+    #[serde(default)]
+    pub replace_recent_blockhash: bool,
+    #[serde(flatten)]
+    pub commitment: Option<CommitmentConfig>,
+    pub encoding: Option<UiTransactionEncoding>,
+    pub accounts: Option<RpcSimulateTransactionAccountsConfig>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct RpcSimulateTransactionResult {
+    pub err: Option<TransactionError>,
+    pub logs: Option<Vec<String>>,
+    pub accounts: Option<Vec<Option<UiAccount>>>,
 }
 
 #[async_trait]
@@ -93,50 +396,66 @@ trait Client {
     async fn get_account_info(
         &self,
         account: Pubkey,
-        slice: Option<AccountSliceConfig>,
+        cfg: Option<RpcAccountInfoConfig>,
     ) -> Result<Account, Error>;
 
     /// https://docs.solana.com/developing/clients/jsonrpc-api#getprogramaccounts
     async fn get_program_accounts(
         &self,
         program: Pubkey,
-        slice: Option<AccountSliceConfig>,
-        filters: Option<&[AccountFilter]>,
-    ) -> Result<Vec<Account>, Error>;
+        cfg: Option<RpcProgramAccountsConfig>,
+    ) -> Result<Vec<RpcKeyedAccount>, Error>;
 
     /// https://docs.solana.com/developing/clients/jsonrpc-api#getmultipleaccounts
     async fn get_multiple_accounts(
         &self,
         accounts: &[Pubkey],
-        slice: Option<AccountSliceConfig>,
+        cfg: Option<RpcAccountInfoConfig>,
     ) -> Result<Vec<Account>, Error>;
 
     /// https://docs.solana.com/developing/clients/jsonrpc-api#getsignaturestatuses
     async fn get_signature_statuses(
         &self,
         signatures: &[Signature],
-        slice: Option<AccountSliceConfig>,
+        cfg: RpcSignatureStatusConfig,
     ) -> Result<Vec<Account>, Error>;
 
     /// https://docs.solana.com/developing/clients/jsonrpc-api#getsignaturesforaddress
-    async fn get_signatures_for_address(&self, address: &Pubkey) -> Result<Vec<Account>, Error>;
+    async fn get_signatures_for_address(
+        &self,
+        address: &Pubkey,
+        cfg: RpcSignaturesForAddressConfig,
+    ) -> Result<Vec<SignatureInfo>, Error>;
 
     /// https://docs.solana.com/developing/clients/jsonrpc-api#getslot
-    async fn get_slot(&self, slice: Option<AccountSliceConfig>) -> u64;
+    async fn get_slot(&self, cfg: Option<RpcSlotConfig>) -> Result<Slot, Error>;
 
     /// https://docs.solana.com/developing/clients/jsonrpc-api#gettransaction
     async fn get_transaction(
         &self,
         signature: Signature,
-        commitment_config: CommitmentConfig,
-    ) -> u64;
+        cfg: Option<RpcTransactionConfig>,
+    ) -> Result<Option<EncodedConfirmedTransaction>, Error>;
 
     /// https://docs.solana.com/developing/clients/jsonrpc-api#requestairdrop
-    async fn request_airdrop(&self, pubkey: &Pubkey, lamports: u64) -> u64;
+    async fn request_airdrop(
+        &self,
+        pubkey: &Pubkey,
+        lamports: u64,
+        commitment: Option<CommitmentConfig>,
+    ) -> Result<String, Error>;
 
     /// https://docs.solana.com/developing/clients/jsonrpc-api#sendtransaction
-    async fn send_transaction(&self, transaction: &Transaction) -> u64;
+    async fn send_transaction(
+        &self,
+        transaction: &Transaction,
+        cfg: RpcSendTransactionConfig,
+    ) -> Result<String, Error>;
 
     /// https://docs.solana.com/developing/clients/jsonrpc-api#simulatetransaction
-    async fn simulate_transaction(&self, transaction: &Transaction) -> u64;
+    async fn simulate_transaction(
+        &self,
+        transaction: &Transaction,
+        cfg: RpcSimulateTransactionConfig,
+    ) -> Result<RpcSimulateTransactionResult, Error>;
 }
